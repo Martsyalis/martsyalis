425541e8fcbfc77812b0c0202f91f8c7829008fa
MaryusMartsyalis <martsyalis@gmail.com>
2018-06-11 19:21:16 -0700

    new build and coming soon modal sign

--- src/blogs/Blogs.js
+++ src/blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import ModalProject from '../projects/ModalProject';
+import ModalProject from './ModalBlog';
+import comingSoon from '../assets/comingSoon.png';
 
 
 export default class Blogs extends PureComponent {
@@ -7,8 +8,7 @@ export default class Blogs extends PureComponent {
   render(){
     return (
       <div>
-        dont have any yet
-        {/* <ModalProject isActive={true}/> */}
+        <ModalProject isActive={true} img={comingSoon} text='Blogs page is still in the works!' />
       </div>
     );
   }
---
+++ src/blogs/ModalBlog.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import './Blogs.css';
+
+
+export default class Project extends PureComponent{
+ 
+  render(){
+    return(
+      <div className={`modal ${this.props.isActive && 'is-active'}`}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{this.props.text}</p>
+          </header>
+          <section className="modal-card-body">
+            <img className='modal-image' src={this.props.img} alt=''/>
+            <div> </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+  
+}
